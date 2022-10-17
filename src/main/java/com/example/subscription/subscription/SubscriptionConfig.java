a461af0f3e7177f332d593eeba9c93e648c2aca0
krunoo53k <petrik.kpetrik@gmail.com>
2022-10-17 16:37:11 +0200

    Connecting do LDAP database

--- src/main/java/com/example/subscription/subscription/SubscriptionConfig.java
+++ src/main/java/com/example/subscription/subscription/SubscriptionConfig.java
@@ -1,4 +1,19 @@
 package com.example.subscription.subscription;
 
+import org.ldaptive.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SubscriptionConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(){
+        return args -> {
+            ConnectionFactory connectionFactory = new SingleConnectionFactory("ldap://10.14.42.6");
+            SearchOperation search = new SearchOperation(connectionFactory, "dc=example,dc=com");
+            SearchResponse response = search.execute();
+            System.out.println(response);
+        };
+    }
 }
