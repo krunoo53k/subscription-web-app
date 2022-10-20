13005ddad9f773ccd93e9c0a62cc23d4be9b3b46
krunoo53k <petrik.kpetrik@gmail.com>
2022-10-20 17:17:01 +0200

    Replacing unecessary code

--- src/main/java/com/example/subscription/subscription/SubscriptionConfig.java
+++ src/main/java/com/example/subscription/subscription/SubscriptionConfig.java
@@ -10,10 +10,7 @@ public class SubscriptionConfig {
     @Bean
     CommandLineRunner commandLineRunner(){
         return args -> {
-            ConnectionFactory connectionFactory = new SingleConnectionFactory("ldap://10.14.42.6");
-            SearchOperation search = new SearchOperation(connectionFactory, "dc=example,dc=com");
-            SearchResponse response = search.execute();
-            System.out.println(response);
+            System.out.println("Starting...");
         };
     }
 }
