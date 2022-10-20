98662ec1a3fe94b08db375bbf12c26b6cc7d34ff
krunoo53k <petrik.kpetrik@gmail.com>
2022-10-20 17:16:28 +0200

    Adding a get request

--- src/main/java/com/example/subscription/subscription/SubscriptionController.java
+++ src/main/java/com/example/subscription/subscription/SubscriptionController.java
@@ -1,4 +1,25 @@
 package com.example.subscription.subscription;
 
+import org.ldaptive.LdapException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "student-test-app/v1")
 public class SubscriptionController {
+    private final SubscriptionService subscriptionService;
+    @Autowired
+    public SubscriptionController(SubscriptionService subscriptionService) { this.subscriptionService = subscriptionService; }
+
+    @GetMapping(path = "{subscriptionId}")
+    public String getSubscription(
+            @PathVariable("subscriptionId") String subscriptionId,
+            @RequestParam(required = false) String  subscribedEvent,
+            @RequestParam(required = false ) String ssnsai
+    ) throws IOException, LdapException {
+    return subscriptionService.getSubscription(subscriptionId, subscribedEvent, ssnsai).toString();
+    }
 }
--- src/main/java/com/example/subscription/subscription/SubscriptionService.java
+++ src/main/java/com/example/subscription/subscription/SubscriptionService.java
@@ -1,4 +1,23 @@
 package com.example.subscription.subscription;
 
+import org.ldaptive.*;
+import org.ldaptive.io.JsonWriter;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.StringWriter;
+@Service
 public class SubscriptionService {
+    public String getSubscription(String subscriptionId, String subscribedEvent, String ssnsai) throws LdapException, IOException {
+        StringWriter writer = new StringWriter();
+        JsonWriter jsonWriter = new JsonWriter(writer);
+        SearchOperation search = new SearchOperation(new DefaultConnectionFactory("ldap://10.14.42.6:389"));
+        SearchResponse response = search.execute(SearchRequest.builder()
+                    .dn("subscriptionId=100,dc=example,dc=com")
+                    .filter("subscriptionId=100")
+                    .build());
+        jsonWriter.write(response);
+        System.out.println(response);
+        return  jsonWriter.toString();
+    }
 }
