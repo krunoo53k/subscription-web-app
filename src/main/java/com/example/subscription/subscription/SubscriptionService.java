6586f2dccf02c823669ad78badc4cf39de71a56e
krunoo53k <petrik.kpetrik@gmail.com>
2022-10-20 17:35:18 +0200

    Adding a not found exception

--- src/main/java/com/example/subscription/subscription/SubscriptionService.java
+++ src/main/java/com/example/subscription/subscription/SubscriptionService.java
@@ -2,7 +2,9 @@ package com.example.subscription.subscription;
 
 import org.ldaptive.*;
 import org.ldaptive.io.JsonWriter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
 import java.io.StringWriter;
@@ -16,6 +18,11 @@ public class SubscriptionService {
                     .dn("subscriptionId="+subscriptionId+",dc=example,dc=com")
                     .filter("subscriptionId="+subscriptionId)
                     .build());
+        if (response.entrySize()<=0){
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "subscription not found"
+            );
+        }
         jsonWriter.write(response);
         System.out.println(writer.toString());
         return  writer.toString();
