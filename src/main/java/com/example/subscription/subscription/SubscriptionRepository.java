58b31ccdd185f6ed6cdf5029665d374e23c247af
krunoo53k <petrik.kpetrik@gmail.com>
2022-10-25 18:05:32 +0200

    Adding a method for adding subscriptions

--- src/main/java/com/example/subscription/subscription/SubscriptionRepository.java
+++ src/main/java/com/example/subscription/subscription/SubscriptionRepository.java
@@ -1,15 +1,26 @@
 package com.example.subscription.subscription;
 
-import org.springframework.data.ldap.repository.LdapRepository;
-import org.springframework.stereotype.Repository;
+import org.ldaptive.LdapEntry;
+import org.ldaptive.beans.reflect.DefaultLdapEntryMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
 
-@Repository
-public interface SubscriptionRepository extends LdapRepository<Subscription> {
-    /*
-        Optional<Subscription> findSubscriptionByDn(String dn){
-        Implementation needed
-        }
-        */
+
+public class SubscriptionRepository{
+
+    public  ResponseEntity addSubscription(Subscription subscription, Integer subscriptionId){
+        //treba se spojit na bazu nekako u repo, to moram skontat kako
+
+        DefaultLdapEntryMapper<Snssai> snssaiDefaultLdapEntryMapper = new DefaultLdapEntryMapper<>();
+        LdapEntry snssaiLdapEntry = new LdapEntry();
+        snssaiDefaultLdapEntryMapper.map(subscription.getSnssai(), snssaiLdapEntry);
+
+        DefaultLdapEntryMapper<Subscription> subscriptionMapper = new DefaultLdapEntryMapper<>();
+        LdapEntry subscriptionLdapEntry = new LdapEntry();
+        subscriptionMapper.map(subscription, subscriptionLdapEntry);
+        subscriptionLdapEntry.setDn("subscriptionId="+ subscriptionId + ",dc=example,dc=com");
+
+        return new ResponseEntity("Request good!", HttpStatus.OK);
+    }
 }
